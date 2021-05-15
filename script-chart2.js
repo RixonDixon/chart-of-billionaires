32bb4a92ce4e00bfd46e7af9b9c15184d8acba5b
RixonDixon <57893720+RixonDixon@users.noreply.github.com>
2021-05-15 12:52:24 +0300

    tool tip on färg på¨lände me text ti höger

--- script-chart2.js
+++ script-chart2.js
@@ -1,7 +1,7 @@
 
 
 
-d3.json("forbes_billionaires.json").then(
+d3.json("/forbes_billionaires.json").then(
    function(d){
        console.log(d);
         createChart(d);
@@ -12,12 +12,21 @@ function createChart(data) {
 
     width= 2000;
     height = 1000;
+
+    tooltip = d3.select('body')
+    .append('div')
+    .style('position', 'absolute')
+    .style('padding', '0 10px')
+    .style('background', 'white')
+    .style('opacity', 0);
+                
   var svg = d3.select('#chart')
     .append('svg')
     .attr('height', height)
     .attr ('width', width)
     .append('g')
-    .attr('transform', 'translate('+ width/2+ ','+height/2+')');
+    .attr('transform', 'translate('+ width/2+ ','+height/2+')')
+   
 
     var radiusScale = d3.scaleSqrt().domain([1, 50]).range([0,35])
 
@@ -26,20 +35,67 @@ function createChart(data) {
     .force('collide', d3.forceCollide(function(d) {
       return radiusScale(d.NetWorth +1)
     }))
-    
+
+   
     var circles = svg.selectAll()
         .data(data)
-        .enter().append('circle')
+        .enter()
+        .append('circle')
         .attr('r',function(d) {
           return radiusScale(d.NetWorth)
         })
-        .attr('fill', 'lightblue')
-        .attr('cx', 100)
-        .attr('cy', 300)
         
+        
+        .attr('fill', function(d){ 
+          if(d.Country == 'United States'){ 
+            return "blue"}
+        if(d.Country == "China"){ return 'red'}})
+
+        .on('mouseover', function(d) {
+          tooltip.transition().duration(200)
+            .style('opacity', .9)
+            .style('visibility', 'visible')
+            console.log(d)
+          tooltip.html(
+            '<div id="dysfunc" style="font-size: 1rem; font-weight: bold; visibility: visible">' +
+               'Name:'+ this.__data__.Name + '<br>' +'Worth:'+ this.__data__.NetWorth + '<br>'+'Selfmade: '+this.__data__.Self_made+ '<br>'+'</div>'
+          )
+            .style('left', (event.pageX -35) + 'px')
+            .style('top', (event.pageY -30) + 'px')
+            .on('mouseout', function(d){
+                this.style.visibility = 'hidden'
+                d3.select("#dysfunc").style('visibility','hidden')
+            })
+          });
+     
          
         simulera.nodes(data)
-        .on('tick', tick)
+        .on('tick', tick);
+      svg.append("text")
+        .attr("x", -430)             
+        .attr("y", -420)
+        .attr("text-anchor", "middle")  
+        .style("font-size", "25px") 
+        .style('fill', 'blue')
+        .style("text-decoration", "underline")  
+        .text("Blue = USA");
+
+        svg.append("text")
+        .attr("x", -430)             
+        .attr("y", -380)
+        .attr("text-anchor", "middle")  
+        .style("font-size", "25px") 
+        .style('fill', 'red')
+        .style("text-decoration", "underline")  
+        .text("Red = China");
+
+        svg.append("text")
+        .attr("x", -430)             
+        .attr("y", -340)
+        .attr("text-anchor", "middle")  
+        .style("font-size", "25px") 
+        .style("text-decoration", "underline")  
+        .text("Black = Other");
 
         function tick(){
         circles
@@ -49,5 +105,9 @@ function createChart(data) {
           .attr('cy', function(d) {
             return d.y
           })
+
+         
+        
+      
 }}
 
